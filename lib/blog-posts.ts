2645a3b59a56542c23c3b84dd9641fae60de6cac
manwelAC <johnmanuelcuerdo@gmail.com>
2026-07-20 16:53:02 +0800

    added more blogs

--- lib/blog-posts.ts
+++ lib/blog-posts.ts
@@ -10,7 +10,29 @@ export const blogPosts: BlogPost[] = [
     slug: "the-beginning",
     title: "The Beginning",
     description: "Blog about my experiences before I graduated in College.",
-    publishedAt: "2026-07-19",
+    publishedAt: "2025-05",
+  },
+
+  {
+    slug:"pre-employment",
+    title:"Pre Employment",
+    description:"Blog about my journey after graduating in College",
+    publishedAt:"2026-04",
+
+  },
+
+  {
+    slug:"employment",
+    title:"I got the Job!",
+    description:"Blog about me taking the full leap of faith",
+    publishedAt:"2026-06",
+  },
+
+  {
+    slug:"virtual-machines",
+    title:"Know Virtual Machines!",
+    description:"Blog about how Virtual Machines should be normalized in workflows.",
+    publishedAt:"2026-06",
   },
 ];
 
